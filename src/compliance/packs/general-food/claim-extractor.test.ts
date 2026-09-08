475a42c12d90c3c3c00efda67b55a867effcc1df
imgaram <ijy918@naver.com>
2026-09-09 00:03:05 +0900

    test: cover diabetes disease phrase variants

--- src/compliance/packs/general-food/claim-extractor.test.ts
+++ src/compliance/packs/general-food/claim-extractor.test.ts
@@ -23,6 +23,10 @@ describe('extractGeneralFoodClaimCandidates', () => {
 
   it.each([
     ['감기 예방에 좋은 차', 'DISEASE_PREVENTION_TREATMENT'],
+    [
+      '일반식품인 이 음료는 당뇨병을 치료합니다',
+      'DISEASE_PREVENTION_TREATMENT',
+    ],
     ['약처럼 빠른 효과의 분말', 'PHARMACEUTICAL_CONFUSION'],
     ['100% 체지방 감소 효과', 'FALSE_EXAGGERATED_CLAIM'],
     [
@@ -39,6 +43,7 @@ describe('extractGeneralFoodClaimCandidates', () => {
 
   it.each([
     '당뇨 환자는 섭취 전 전문가와 상담하세요.',
+    '당뇨병을 치료하는 환자는 섭취 전 전문가와 상담하세요.',
     '영양정보: 당류 0g',
     '의사가 추천하지 않았습니다.',
     '전후 사진을 사용하지 않습니다.',
